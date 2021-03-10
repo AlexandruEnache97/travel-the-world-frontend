a2fd14264071f5b2a25c72f907185e2aa6c8face
unknown <alexenache43@yahoo.com>
2021-03-10 16:35:11 +0200

    resolve-import

--- src/pages/LandingPage/LandingContainer.jsx
+++ src/pages/LandingPage/LandingContainer.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import mountains from '../../images/Flat-Mountains.svg';
 import logo from '../../images/logo_planet.png';
-import Register from './components/Register';
 import SignIn from './components/SignIn';
 
 const LandingContainer = () => (
