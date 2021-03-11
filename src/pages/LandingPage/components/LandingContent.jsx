b948b1b17ac9d47dd417a044e346baa689f37e7c
unknown <alexenache43@yahoo.com>
2021-03-11 12:14:04 +0200

    configure-react-router

--- src/pages/LandingPage/components/LandingContent.jsx
+++ src/pages/LandingPage/components/LandingContent.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const LandingContent = () => (
   <div>
     <span className="content-description">Share your travel experiences and enhance new ones by enjoying this world together!</span>
     <span className="content-text">Conquer the world and discover it!</span>
-    <button className="content-signIn" type="button">Sign In</button>
-    <button className="content-signUp" type="button">Sign Up</button>
+    <Link to="/login" className="button content-signIn" type="button">Sign In</Link>
+    <Link to="/register" className="button content-signUp" type="button">Sign Up</Link>
   </div>
 );
 
