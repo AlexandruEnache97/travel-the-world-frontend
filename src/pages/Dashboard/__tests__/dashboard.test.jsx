5516bff99fa52bf833e5ff7ecbcf9da4d72420ef
unknown <alexenache43@yahoo.com>
2021-03-22 14:40:20 +0200

    repair-dashboard-unit-test

--- src/pages/Dashboard/__tests__/dashboard.test.jsx
+++ src/pages/Dashboard/__tests__/dashboard.test.jsx
@@ -1,16 +1,23 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import '@testing-library/jest-dom';
+import { MemoryRouter } from 'react-router-dom';
 import Dashboard from '../Dashboard';
 
 describe('Dashboard component tests', () => {
   it('Should render component correctly', () => {
     const getPosts = jest.fn();
+    const signOut = jest.fn();
     const auth = { accountData: { username: 'Alex' } };
-    render(<Dashboard
-      auth={auth}
-      getPosts={getPosts}
-      posts={{ currentPosts: [] }}
-    />);
+    render(
+      <MemoryRouter>
+        <Dashboard
+          auth={auth}
+          getPosts={getPosts}
+          posts={{ currentPosts: [] }}
+          signOut={signOut}
+        />
+      </MemoryRouter>,
+    );
   });
 });
