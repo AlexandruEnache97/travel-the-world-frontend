75b2e1772d4baa540e4676bd65ae02fe88aefd7c
unknown <alexenache43@yahoo.com>
2021-03-31 14:46:05 +0300

    removed-console-logs

--- src/pages/Dashboard/components/comments/CommentList.jsx
+++ src/pages/Dashboard/components/comments/CommentList.jsx
@@ -1,29 +1,22 @@
 /* eslint-disable react/prop-types */
-import React, { useEffect } from 'react';
+import React from 'react';
 import CommentComponent from './CommentComponent';
 
 const CommentList = ({
   username, postId, comments, totalResults, getMoreComments, deleteComment,
-}) => {
-  useEffect(() => {
-    console.log(totalResults);
-  }, [totalResults]);
-  return (
-    <div className="comments-list-container" id={`comment-id-${postId}`}>
-      {totalResults > 0 ? comments.map((comment) => {
-        console.log(`${comment.userData.username} ${username}`);
-        return (
-          <CommentComponent
-            key={Math.random()}
-            commentId={comment._id}
-            profileImage={comment.userData.profileImage}
-            username={comment.userData.username}
-            text={comment.text}
-            access={comment.userData.username === username}
-            deleteComment={deleteComment}
-          />
-        );
-      }) : totalResults === 0
+}) => (
+  <div className="comments-list-container" id={`comment-id-${postId}`}>
+    {totalResults > 0 ? comments.map((comment) => (
+      <CommentComponent
+        key={Math.random()}
+        commentId={comment._id}
+        profileImage={comment.userData.profileImage}
+        username={comment.userData.username}
+        text={comment.text}
+        access={comment.userData.username === username}
+        deleteComment={deleteComment}
+      />
+    )) : totalResults === 0
       && (
       <div className="comments-loading">
         <span>Loading</span>
@@ -32,18 +25,17 @@ const CommentList = ({
         </div>
       </div>
       )}
-      <div className="comments-more">
-        {totalResults > comments.length && (
+    <div className="comments-more">
+      {totalResults > comments.length && (
         <button type="button" onClick={getMoreComments}>
           Load
           {' '}
           {totalResults - comments.length < 10 ? totalResults - comments.length : 10}
           {' more comments'}
         </button>
-        )}
-      </div>
+      )}
     </div>
-  );
-};
+  </div>
+);
 
 export default CommentList;
