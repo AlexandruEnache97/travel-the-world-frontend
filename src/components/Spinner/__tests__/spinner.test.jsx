11025dc818ac09e28c53a9eb0b4961ee0f69bcaa
unknown <alexenache43@yahoo.com>
2021-04-09 10:32:31 +0300

    post-calculate-date-and-spinner-unit-tests

---
+++ src/components/Spinner/__tests__/spinner.test.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import Spinner from '../Spinner';
+
+describe('Spinner component tests', () => {
+  it('Should render component correctly', () => {
+    render(
+      <Spinner />,
+    );
+  });
+});
