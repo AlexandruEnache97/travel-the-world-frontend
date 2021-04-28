ea7b75550776932ef20d3eefd7e0c0b5d829b66f
unknown <alexenache43@yahoo.com>
2021-04-28 16:40:32 +0300

    add-prop-types-to-protected-routes

--- src/components/ProtectedRoute/ProtectedRoute.jsx
+++ src/components/ProtectedRoute/ProtectedRoute.jsx
@@ -1,6 +1,6 @@
-/* eslint-disable react/prop-types */
 /* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Redirect, Route } from 'react-router-dom';
 
 const ProtectedRoute = ({ component: Component, auth, ...rest }) => {
@@ -15,6 +15,24 @@ const ProtectedRoute = ({ component: Component, auth, ...rest }) => {
       ))}
     />
   );
+};
+
+ProtectedRoute.propTypes = {
+  component: PropTypes.oneOfType([
+    PropTypes.object,
+    PropTypes.array,
+  ]).isRequired,
+  auth: PropTypes.shape({
+    accountData: PropTypes.shape({
+      username: PropTypes.string.isRequired,
+      email: PropTypes.string.isRequired,
+      profileImage: PropTypes.string.isRequired,
+      country: PropTypes.string.isRequired,
+      userLocation: PropTypes.objectOf(PropTypes.number).isRequired,
+    }).isRequired,
+    accountId: PropTypes.string.isRequired,
+    isAuthenticated: PropTypes.bool.isRequired,
+  }).isRequired,
 };
 
 export default ProtectedRoute;
