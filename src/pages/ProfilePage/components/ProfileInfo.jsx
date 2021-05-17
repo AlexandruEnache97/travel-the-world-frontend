79fd463598625bbcbf03416cceaa4128f4b60452
unknown <alexenache43@yahoo.com>
2021-05-17 12:12:03 +0300

    profile-image-fit-container

--- src/pages/ProfilePage/components/ProfileInfo.jsx
+++ src/pages/ProfilePage/components/ProfileInfo.jsx
@@ -1,25 +1,37 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import './profileInfo.scss';
 
-const ProfileInfo = ({ currentUser }) => (
+const ProfileInfo = ({ currentUser }) => {
+  const profileImage = useRef();
 
-  <div className="profile-user" id="profileRef">
-    <div className="profile">
-      <div className="user-image">
-        <img src={currentUser.profileImage} alt="" />
+  useEffect(() => {
+    console.log(`${profileImage.current.naturalWidth} ${profileImage.current.naturalHeight}`);
+  }, []);
+
+  return (
+    <div className="profile-user" id="profileRef">
+      <div className="profile">
+        <div className="user-image">
+          <img
+            ref={profileImage}
+            src={currentUser.profileImage}
+            alt=""
+          />
+
+        </div>
+        <p className="user-name">{currentUser.username}</p>
+        <p className="user-country">
+          Country:
+          {'  '}
+          {currentUser.country}
+        </p>
+        <button className="user-edit" type="button">Edit profile</button>
+        <button className="user-edit" type="button">Map</button>
       </div>
-      <p className="user-name">{currentUser.username}</p>
-      <p className="user-country">
-        Country:
-        {'  '}
-        {currentUser.country}
-      </p>
-      <button className="user-edit" type="button">Edit profile</button>
-      <button className="user-edit" type="button">Map</button>
     </div>
-  </div>
-);
+  );
+};
 
 ProfileInfo.propTypes = {
   currentUser: PropTypes.shape({
