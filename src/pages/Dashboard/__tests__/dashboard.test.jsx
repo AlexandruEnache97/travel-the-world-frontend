050efd9e662db17492df7f785cf6440fe61b4378
unknown <alexenache43@yahoo.com>
2021-05-24 11:04:15 +0300

    repair-dashboard-unit-test

--- src/pages/Dashboard/__tests__/dashboard.test.jsx
+++ src/pages/Dashboard/__tests__/dashboard.test.jsx
@@ -6,21 +6,43 @@ import Dashboard from '../Dashboard';
 
 jest.mock('../components/posts/CreatePost.jsx', () => () => <p>Create Post</p>);
 jest.mock('../components/menu/MenuButton.jsx', () => () => <p>Menu Button</p>);
+jest.mock('../../../components/Buttons/ScrollButton', () => () => <p>Scroll</p>);
+jest.mock('../../../components/Alerts/ConnectedAlert.jsx', () => () => <p>Alert</p>);
 
 describe('Dashboard component tests', () => {
   it('Should render component correctly', () => {
     const getPosts = jest.fn();
     const getLikedPosts = jest.fn();
     const signOut = jest.fn();
-    const auth = { accountData: { username: 'Alex' } };
+    const auth = {
+      accountData: {
+        username: 'Alex',
+        email: 'alex@gmail.com',
+        profileImage: '',
+        country: 'Romania',
+        userLocation: {
+          lat: 0,
+          lng: 0,
+        },
+      },
+      accountId: '1',
+      isAuthenticated: true,
+    };
     render(
       <MemoryRouter>
         <Dashboard
           auth={auth}
           getPosts={getPosts}
           getLikedPosts={getLikedPosts}
-          posts={{ currentPosts: [] }}
+          posts={{
+            currentPosts: [],
+            likedPosts: [],
+            singlePost: {},
+            totalResults: 0,
+          }}
           signOut={signOut}
+          createAlert={() => { }}
+          createPost={() => { }}
         />
       </MemoryRouter>,
     );
