0068e951be3af7a2cad023d7e5d655422a70f149
Alexandru-Gabriel Enache <alexandru.enache@fortech.ro>
2021-05-31 19:32:21 +0300

    comments-modal-unit-test

--- src/pages/Dashboard/components/comments/__tests__/commentsModal.test.jsx
+++ src/pages/Dashboard/components/comments/__tests__/commentsModal.test.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
-import { render, waitFor } from '@testing-library/react';
+import {
+  fireEvent, render, screen, waitFor,
+} from '@testing-library/react';
+import '@testing-library/jest-dom';
 import axios from 'axios';
 import MockAdapter from 'axios-mock-adapter';
 import CommentsModal from '../CommentsModal';
@@ -7,7 +10,7 @@ import { NODE_SERVER } from '../../../../../../config';
 
 const auth = {
   accountData: {
-    username: 'test',
+    username: 'CatUser',
     email: 'test@gmail.com',
     country: 'France',
     profileImage: 'link',
@@ -34,23 +37,70 @@ const mock = new MockAdapter(axios, { onNoMatch: 'throwException' });
 describe('Comments modal tests', () => {
   it('Test get comments from backend', async () => {
     await waitFor(() => {
-      mock.onGet(`${NODE_SERVER.baseUrl}/api/getComments/1/1`).reply(200, {
+      mock.onGet(`${NODE_SERVER.baseUrl}/api/getComments/607930931a05f60023c1ef40/1`).reply(200, {
         results: mockPost,
         totalResults: 11,
       });
-      mock.onGet(`${NODE_SERVER.baseUrl}/api/likedComments/1/1`).reply(200, {
+      mock.onGet(`${NODE_SERVER.baseUrl}/api/likedComments/607930931a05f60023c1ef40/1`).reply(200, {
         likedComments: [
           '6079459a8c0bc9002321006f',
           '6079459a8c0bc900232100gg',
         ],
       });
-      render(<CommentsModal postId="1" auth={auth} postUser="test2" />);
+      mock.onDelete(`${NODE_SERVER.baseUrl}/api/deleteComment`).reply(200);
+      render(<CommentsModal postId="607930931a05f60023c1ef40" auth={auth} postUser="test2" />);
     });
   });
   it('Should catch empty result from backend', async () => {
     await waitFor(() => {
       mock.onGet(`${NODE_SERVER.baseUrl}/api/getComments/1/1`).reply(404);
       render(<CommentsModal postId="1" auth={auth} postUser="test2" />);
+    });
+  });
+
+  it('Should delete comment from post', async () => {
+    await waitFor(() => {
+      mock.onGet(`${NODE_SERVER.baseUrl}/api/getComments/607930931a05f60023c1ef40/1`).reply(200, {
+        results: mockPost,
+        totalResults: 11,
+      });
+      mock.onGet(`${NODE_SERVER.baseUrl}/api/likedComments/607930931a05f60023c1ef40/1`).reply(200, {
+        likedComments: [
+          '6079459a8c0bc9002321006f',
+          '6079459a8c0bc900232100gg',
+        ],
+      });
+      mock.onDelete(`${NODE_SERVER.baseUrl}/api/deleteComment`).reply(200);
+      render(<CommentsModal postId="607930931a05f60023c1ef40" auth={auth} postUser="test2" />);
+
+      const deleteComment = screen.getByRole('button', {
+        name: 'Delete',
+      });
+      expect(deleteComment).toBeInTheDocument();
+      fireEvent.click(deleteComment);
+    });
+  });
+
+  it('Should delete last comment from post', async () => {
+    await waitFor(() => {
+      mock.onGet(`${NODE_SERVER.baseUrl}/api/getComments/607930931a05f60023c1ef40/1`).reply(200, {
+        results: mockPost,
+        totalResults: 1,
+      });
+      mock.onGet(`${NODE_SERVER.baseUrl}/api/likedComments/607930931a05f60023c1ef40/1`).reply(200, {
+        likedComments: [
+          '6079459a8c0bc9002321006f',
+          '6079459a8c0bc900232100gg',
+        ],
+      });
+      mock.onDelete(`${NODE_SERVER.baseUrl}/api/deleteComment`).reply(200);
+      render(<CommentsModal postId="607930931a05f60023c1ef40" auth={auth} postUser="test2" />);
+
+      const deleteComment = screen.getByRole('button', {
+        name: 'Delete',
+      });
+      expect(deleteComment).toBeInTheDocument();
+      fireEvent.click(deleteComment);
     });
   });
 });
